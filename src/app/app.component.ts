25d3070e195b379bc6c171d419441b9962136f87
Alvaro Valero <alvaro.valero.garcia.al@iespoligonosur.org>
2025-02-06 14:36:17 +0100

    Subir componentes nuevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import e from 'express';
-import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
 
-  imports: [MenuComponent, HomeComponent, RouterOutlet],
+  imports: [ HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
