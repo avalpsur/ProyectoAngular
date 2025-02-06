32b3cff52fbbca97936cefabdb1394902aee17c3
Alvaro Valero <alvaro.valero.garcia.al@iespoligonosur.org>
2025-02-06 19:51:41 +0100

    Subir web

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+  comics = [
+    {
+      title: 'Constantine Reaches Out For Help Moving A Tricky Demon in Birds Of Prey #18',
+      date: 'February 4, 2025',
+      image: 'assets/images/comic1.jpg'
+    },
+    {
+      title: 'Johnny Blaze Reels From One Mind-Bending Discovery After Another Spirits of Vengeance #6',
+      date: 'February 4, 2025',
+      image: 'assets/images/comic2.jpg'
+    },
+    {
+      title: 'Cassandra Cain, Caught In The Middle Of The Deadliest Hands In The DC Universe in Batgirl #4',
+      date: 'February 4, 2025',
+      image: 'assets/images/comic3.jpg'
+    },
+    {
+      title: 'Felicia Hardy Setd Sights On Wanda’s Treasure Trove Of Magical Items in Scarlet Witch #9',
+      date: 'February 4, 2025',
+      image: 'assets/images/comic4.jpg'
+    }
+  ];
+}
